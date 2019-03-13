bbdf3c43d63f67caffe8a91bfa474524458b47d5
Quanhu Sheng <shengqh@gmail.com>
2019-03-13 14:52:59 -0500

    setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # annotateGenome
+
+This package is used to annotate bed file with pre-calculated genome information.
--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
-from setuptools import setup, find_packages
-setup(name='annotateGenome',
-      version='0.0.1',
-      packages = find_packages(),
-      )
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="annotateGenome_shengqh",
+    version="0.0.1",
+    author="Quanhu Sheng",
+    author_email="quanhu.sheng.1@vumc.org",
+    description="Annotate bed file with genome information",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shengqh/annotateGenome",
+    entry_points = {
+        'console_scripts': ['annotateGenome=annotateGenome.__main__:main'],
+    },
+    packages=setuptools.find_packages(),
+    install_requires=['argparse', 'pytabix', 'numpy'],
+    data_files=[('bin', ['bin/bedClip','bin/bedGraphToBigWig'])],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False
+)
+
