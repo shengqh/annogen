03321639deb1751d8ded01a32ef44736912d3c68
Quanhu Sheng <shengqh@gmail.com>
2019-04-23 11:36:24 -0500

    merge with Hui's version

--- src/annotateGenome/data/R_comp.R
+++ src/annotateGenome/data/R_comp.R
@@ -1,42 +1,48 @@
 #!/usr/bin/env Rscript
 
-#sudo apt-get update && sudo apt-get install pandoc
+library("optparse")
+parser <- OptionParser()
+parser <- add_option(parser, c("-c", "--control"), action="store", help="Control file")
+parser <- add_option(parser, c("--controlName"), action="store", help="Control name")
+parser <- add_option(parser, c("-s", "--sample"), action="store", help="Sample file")
+parser <- add_option(parser, c("--sampleName"), action="store", help="Sample name")
+parser <- add_option(parser, c("-d", "--databaseFolder"), action="store", help="Database folder")
+parser <- add_option(parser, c("-o", "--output"), action="store", help="Output html file")
+opts<-parse_args(parser, commandArgs(trailing=TRUE))
 
-require(docopt)
-'Usage:
-R_comp.R [-c <control> --controlName <controlName> -s <sample> --sampleName <sampleName> -r <rmdFile> -d <databaseFolder> -o <output>]
+check_file<-function(fileName){
+  if (!file.exists(fileName)){
+    stop(paste0("File not exists : ", fileName))
+  }
+}
 
-Options:
--h --help     Show this screen.
--c            Control file.
---controlName Control name.
--s            Sample file.
---sampleName  Sample name.
--r            RMD report file
--d            Database folder
--o            Output html file 
+check_file(opts$control)
+check_file(opts$sample)
+check_file(opts$databaseFolder)
 
-]' -> doc
-
-opts <- docopt(doc)
+args<-commandArgs(trailing=FALSE)
+scriptFile=args[grepl("--file=", args)]
+scriptFile<-gsub("--file=","",scriptFile)
+scriptFile<-normalizePath(scriptFile)
+rmdFile<-gsub(".R$", ".Rmd", scriptFile)
 
 options<-list(control = normalizePath(opts$control),
               controlName = opts$controlName,
               sample = normalizePath(opts$sample),
               sampleName = opts$sampleName,
-              rmdFile = normalizePath(opts$rmdFile),
+              rmdFile = rmdFile,
               databaseFolder = normalizePath(opts$databaseFolder),
               outputFile = normalizePath(opts$output))
 
 print(options)
 
 library(rmarkdown)
-reportRmd<-opts$rmdFile
+reportRmd<-options$rmdFile
 
-output_dir = dirname(opts$output)
-output_file = basename(opts$output)
+output_dir = dirname(options$output)
+output_file = basename(options$output)
 
-cat("Output report to:", opts$output, "\n")
+cat("Output report to:", options$output, "\n")
 rmarkdown::render(reportRmd,
                   output_dir = output_dir,
                   output_file = output_file,
